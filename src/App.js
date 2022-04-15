2a9b54b21a56811f91db4a255e7fe8df4b5001f5
Somesh <Somesh.4543@gmail.com>
2022-04-15 16:23:42 +0530

    Links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Product from "./Routes/Products";
+import Welcome from "./Routes/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Product />
+        </Route>
+      </main>
     </div>
   );
 }
